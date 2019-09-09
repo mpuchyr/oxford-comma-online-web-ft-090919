dc95cea8beafa068d7eef9654ad5850c3ef7c7bd
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-09 23:00:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  string = ""
+  
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    array.each do |item|
+      if item == array[-1]
+        string << "and " + item
+      else
+        string << item + ", "
+      end
+    end
+  end
+  
+  string
 end
