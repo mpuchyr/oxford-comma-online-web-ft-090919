4aa59f57421675060766d4cfaabe8812564885ff
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-09 22:58:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,16 @@
 def oxford_comma(array)
+  string = ""
+  
   if array.length == 1
     return array[0]
   elsif array.length == 2
     array.join(" and ")
   else
-    
+    array.each do |item|
+      if item == array[-1]
+        string << "and " + item
+      else
+        string << item + ", "
+      end
   end
 end
